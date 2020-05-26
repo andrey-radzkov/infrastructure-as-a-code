807015f5a3bc04274e734a06a4ae71936a0cdb4c
liniyakamnya <liniyakamnya@gmail.com>
2020-05-26 08:52:22 +0300

    public DNS

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-//output "security_group" {
-//  value = "${join(", ", aws_security_group.default.*.id)}"
-//}
-//
-output "web_ip" {
-  value = "${aws_instance.example.public_ip}"
+output "public_dns" {
+  value = "${aws_instance.example.public_dns}"
 }
-//
-//output "elb_address" {
-//  value = "${aws_elb.web.dns_name}"
-//}
