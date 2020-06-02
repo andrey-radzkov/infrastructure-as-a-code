e8363d02269d5dd219b6d51b9ad72b0434e1bac2
liniyakamnya <liniyakamnya@gmail.com>
2020-06-02 20:07:45 +0300

    register instances automatically

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -1,12 +1,17 @@
 package com.demo;
 
-import com.amazonaws.services.elasticloadbalancingv2.AmazonElasticLoadBalancing;
-import com.amazonaws.services.elasticloadbalancingv2.AmazonElasticLoadBalancingClientBuilder;
+import com.amazonaws.services.elasticloadbalancing.AmazonElasticLoadBalancing;
+import com.amazonaws.services.elasticloadbalancing.AmazonElasticLoadBalancingClientBuilder;
+import com.amazonaws.services.elasticloadbalancing.model.Instance;
+import com.amazonaws.services.elasticloadbalancing.model.RegisterInstancesWithLoadBalancerRequest;
+import com.amazonaws.util.EC2MetadataUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @RestController
@@ -15,6 +20,10 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         AmazonElasticLoadBalancing loadBalancing = AmazonElasticLoadBalancingClientBuilder.defaultClient();
+        RegisterInstancesWithLoadBalancerRequest instanceRequest = new RegisterInstancesWithLoadBalancerRequest();
+        instanceRequest.setLoadBalancerName("tf-example-lb-ec2");
+        instanceRequest.setInstances(Collections.singletonList(new Instance(EC2MetadataUtils.getInstanceId())));
+        loadBalancing.registerInstancesWithLoadBalancer(instanceRequest);
     }
 
     @GetMapping("/hello")
