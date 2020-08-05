a5c760bd624deab5cc30badb3dc14879d394c07c
liniyakamnya <liniyakamnya@gmail.com>
2020-08-05 08:58:54 +0300

    create queue via java api

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
+import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
 
 @SpringBootApplication
 @RestController
@@ -17,6 +22,28 @@ public class DemoApplication {
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/queue")
+    public String queue(@RequestParam(value = "queue-name", defaultValue = "World") String queueName) {
+        System.out.println("\nCreate queue");
+        // snippet-start:[sqs.java2.sqs_example.create_queue]
+        SqsClient sqsClient = SqsClient.builder()
+                .region(Region.US_WEST_2)
+                .build();
+        CreateQueueRequest createQueueRequest = CreateQueueRequest.builder()
+                .queueName(queueName)
+                .build();
+
+        sqsClient.createQueue(createQueueRequest);
+        // snippet-end:[sqs.java2.sqs_example.create_queue]
+
+        System.out.println("\nGet queue URL");
+        // snippet-start:[sqs.java2.sqs_example.get_queue]
+        GetQueueUrlResponse getQueueUrlResponse =
+                sqsClient.getQueueUrl(GetQueueUrlRequest.builder().queueName(queueName).build());
+        String queueUrl = getQueueUrlResponse.queueUrl();
+        return queueUrl;
     }
 
     @GetMapping("/")
