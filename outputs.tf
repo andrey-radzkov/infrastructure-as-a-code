decc4dbab7a968eb17ec99a92f39b98e0dc4ce76
Andrey_Radzkov <andrey.radzkov@gmail.com>
2021-04-05 16:08:53 +0300

    exception handling

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "public_dns" {
-  value = "http://${aws_instance.example.public_dns}:8080/set-name?name=newName"
+  value = "http://${aws_instance.example.public_dns}:8080/order"
 }
--- src/main/java/com/demo/ExceptionHandlingController.java
+++ src/main/java/com/demo/ExceptionHandlingController.java
@@ -3,11 +3,12 @@ package com.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
+@ControllerAdvice
 public class ExceptionHandlingController {
     private static final Logger log = LoggerFactory.getLogger(ExceptionHandlingController.class);
 
