7f2e8c8fb0b509e0595628751c05419dd6de9682
Andrey_Radzkov <andrey.radzkov@gmail.com>
2021-04-05 16:21:55 +0300

    exception handling

--- src/main/java/com/demo/ExceptionHandlingController.java
+++ src/main/java/com/demo/ExceptionHandlingController.java
@@ -2,17 +2,21 @@ package com.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionHandlingController {
     private static final Logger log = LoggerFactory.getLogger(ExceptionHandlingController.class);
 
     @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleError(HttpServletRequest req, Exception ex) {
         log.error("Request: " + req.getRequestURL() + " raised " + ex);
         return ex.getMessage();
