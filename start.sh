0a2693011fde96f17e1b0fcab678313a9684b4c7
Andrey_Radzkov <andrey.radzkov@gmail.com>
2021-10-05 10:56:44 +0300

    java 17, gcp

--- start.sh
+++ start.sh
@@ -5,8 +5,12 @@ wget https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.deb
 sudo apt-get -y install git
 sudo apt -y  install ./jdk-17_linux-x64_bin.deb
 sudo git clone --single-branch --branch=feature/gcp https://github.com/andrey-radzkov/infrastructure-as-a-code.git
+
 sudo apt-get -y install nginx
+sudo cp /infrastructure-as-a-code/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo cp /infrastructure-as-a-code/nginx/nginx.conf /etc/init/nginx.conf
 sudo /etc/init.d/nginx start
+
 cd infrastructure-as-a-code/
 sudo chmod 777 gradlew
 sudo ./gradlew build
